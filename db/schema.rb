6094680779176dc0476ec44bd83baa9c1d5c50a1
Valeriya <VS.Kareva@dpo-stbt.ru>
2021-02-05 17:35:30 +0500

    user.background

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_112934) do
+ActiveRecord::Schema.define(version: 2021_02_04_125219) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_112934) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "background"
   end
 
   add_foreign_key "questions", "users"
